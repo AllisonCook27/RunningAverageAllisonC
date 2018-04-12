9097b507d3f67df87e37c7e9e89886c98982bfff
AllisonCook27 <allison.cook@ocsbstudent.ca>
2018-04-12 14:29:53 -0400

    stuff

--- RunningAverageAllisonC/RunningAverageForm.Designer.cs
+++ RunningAverageAllisonC/RunningAverageForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace RunningAverageAllisonC
 {
-    partial class Form1
+    partial class frmRunningAverage
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,85 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblExit = new System.Windows.Forms.Label();
+            this.lblNumber = new System.Windows.Forms.Label();
+            this.txtNumbers = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lblAverage = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblExit
+            // 
+            this.lblExit.AutoSize = true;
+            this.lblExit.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblExit.Location = new System.Drawing.Point(12, 23);
+            this.lblExit.Name = "lblExit";
+            this.lblExit.Size = new System.Drawing.Size(119, 18);
+            this.lblExit.TabIndex = 0;
+            this.lblExit.Text = "To exit enter -1";
+            // 
+            // lblNumber
+            // 
+            this.lblNumber.AutoSize = true;
+            this.lblNumber.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumber.Location = new System.Drawing.Point(12, 78);
+            this.lblNumber.Name = "lblNumber";
+            this.lblNumber.Size = new System.Drawing.Size(263, 18);
+            this.lblNumber.TabIndex = 1;
+            this.lblNumber.Text = "Enter a number between 1 and 100:";
+            // 
+            // txtNumbers
+            // 
+            this.txtNumbers.Location = new System.Drawing.Point(301, 76);
+            this.txtNumbers.Name = "txtNumbers";
+            this.txtNumbers.Size = new System.Drawing.Size(100, 20);
+            this.txtNumbers.TabIndex = 2;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(27, 135);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(88, 30);
+            this.btnCalculate.TabIndex = 3;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // lblAverage
+            // 
+            this.lblAverage.AutoSize = true;
+            this.lblAverage.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAverage.Location = new System.Drawing.Point(37, 207);
+            this.lblAverage.Name = "lblAverage";
+            this.lblAverage.Size = new System.Drawing.Size(0, 18);
+            this.lblAverage.TabIndex = 4;
+            // 
+            // frmRunningAverage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.Teal;
+            this.ClientSize = new System.Drawing.Size(423, 261);
+            this.Controls.Add(this.lblAverage);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtNumbers);
+            this.Controls.Add(this.lblNumber);
+            this.Controls.Add(this.lblExit);
+            this.Name = "frmRunningAverage";
+            this.Text = "Running Average by Allison C";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblExit;
+        private System.Windows.Forms.Label lblNumber;
+        private System.Windows.Forms.TextBox txtNumbers;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblAverage;
     }
 }
 
--- RunningAverageAllisonC/RunningAverageForm.cs
+++ RunningAverageAllisonC/RunningAverageForm.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/*
+ * Created by: Allison Cook
+ * Created on: 12 April, 2018
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #29 - Running Average
+ * This program calculates the average of a growing collection of numbers
+*/
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,11 +17,25 @@ using System.Windows.Forms;
 
 namespace RunningAverageAllisonC
 {
-    public partial class Form1 : Form
+    public partial class frmRunningAverage : Form
     {
-        public Form1()
+        public frmRunningAverage()
         {
             InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            //variable
+            int userNumber, sum, total = 0;
+            double average;
+
+            //add one to total
+            total = total + 1;
+            //asign the input
+            userNumber = Convert.ToInt32(txtNumbers);
+
+            //add it to sum
         }
     }
 }
